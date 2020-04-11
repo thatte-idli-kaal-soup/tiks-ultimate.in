696539a9fecc8ed2ef57198fea4913573abb2e63
Puneeth Chaganti <punchagan@muse-amuse.in>
2020-04-11 15:18:50 +0530

    Make some minor changes to the configuration

--- config.js
+++ config.js
@@ -1,35 +1,44 @@
+const description = 'Ultimate Frisbee Sports Club from South Bangalore';
 module.exports = {
-  siteTitle: 'Gatsby Starter Stylish', // <title>
-  manifestName: 'Stylish',
-  manifestShortName: 'Landing', // max 12 characters
-  manifestStartUrl: '/',
-  manifestBackgroundColor: '#663399',
-  manifestThemeColor: '#663399',
-  manifestDisplay: 'standalone',
-  manifestIcon: 'src/assets/img/website-icon.png',
-  pathPrefix: `/gatsby-starter-stylish/`, // This path is subpath of your hosting https://domain/portfolio
-  heading: 'Anubhav Srivastava',
-  subHeading: 'Full time Web Developer. Part time Open source contributor  ',
+    siteTitle: 'Thatte Idli Kaal Soup', // <title>
+    siteDescription: description,
+    siteKeywords:
+        'Ultimate Frisbee, Bangalore, Jayanagar, South Bangalore, Fitness, Sports, Frisbee, Friends, Sports Club, TIKS, Thatte Idli Kaal Soup',
+    manifestName: 'Stylish',
+    manifestShortName: 'Landing', // max 12 characters
+    manifestStartUrl: '/',
+    manifestBackgroundColor: '#663399',
+    manifestThemeColor: '#663399',
+    manifestDisplay: 'standalone',
+    manifestIcon: 'src/assets/img/website-icon.png',
+    pathPrefix: `/`, // This path is subpath of your hosting https://domain/portfolio
+    heading: 'Thatte Idli Kaal Soup',
+    subHeading: description,
 
-  // social
-  socialLinks: [
-    {
-      icon: 'icon-social-github',
-      name: 'Github',
-      url: 'https://github.com/anubhavsrivastava',
-    },
-    {
-      icon: 'icon-social-twitter',
-      name: 'Twitter',
-      url: 'https://twitter.com/onlyanubhav',
-    },
-    {
-      icon: 'icon-social-facebook',
-      name: 'Facebook',
-      url: 'https://facebook.com/theanubhav',
-    },
-  ],
-  email: 'anubhav.srivastava00@gmail.com',
-  phone: '000-00000',
-  address: '305 Del Sol, Sunnyvale, CA',
+    // social
+    socialLinks: [
+        {
+            icon: 'icon-social-instagram',
+            name: 'Instagram',
+            url: 'https://instagram.com/thatteidlikaalsoup'
+        },
+        {
+            icon: 'icon-social-facebook',
+            name: 'Facebook',
+            url: 'https://facebook.com/Thatte-Idli-Kaal-Soup-448323595322728'
+        },
+        {
+            icon: 'icon-social-email',
+            name: 'Email',
+            url: 'tiks.india.ultimate@gmail.com'
+        },
+        {
+            icon: 'icon-social-github',
+            name: 'Github',
+            url: 'https://github.com/thatte-idli-kaal-soup'
+        }
+    ],
+    email: 'tiks.india.ultimate@gmail.com',
+    phone: '000-00000',
+    address: 'Jayanagar, Banglaore, India'
 };
