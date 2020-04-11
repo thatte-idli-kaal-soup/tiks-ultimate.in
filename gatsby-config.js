a84f3d4aefd79c7e2d8cf657c6f1c436e736d12d
Puneeth Chaganti <punchagan@muse-amuse.in>
2020-04-11 16:05:40 +0530

    gatsby-config: Add configuration to fetch posts from RSVP app

--- gatsby-config.js
+++ gatsby-config.js
@@ -22,6 +22,45 @@ module.exports = {
             }
         },
         'gatsby-plugin-sass',
-        'gatsby-plugin-offline'
+        'gatsby-plugin-offline',
+        {
+            resolve: 'gatsby-source-apiserver',
+            options: {
+                // Type prefix of entities from server
+                typePrefix: 'blog__',
+
+                // The url, this should be the endpoint you are attempting to pull data from
+                url: `http://localhost:5000/api/posts`,
+
+                method: 'get',
+
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+
+                // Name of the data to be downloaded.  Will show in graphQL or be saved to a file
+                // using this name. i.e. posts.json
+                name: `posts`,
+
+                // Optionally include some output when building
+                // Default is false
+                verboseOutput: true, // For debugging purposes
+
+                // Optionally re-source data when it changes and
+                // `gatsby develop` is running.
+                // Requires `ENABLE_GATSBY_REFRESH_ENDPOINT=true`.
+                // See https://www.gatsbyjs.org/docs/environment-variables/#reserved-environment-variables
+                // Default is false
+                enableDevRefresh: true,
+
+                // Optionally override key used to re-source data
+                // when `gatsby develop` is running.
+                // Requires `enableDevRefresh: true`.
+                // See setting directly above this one.
+                // See also https://github.com/gatsbyjs/gatsby/issues/14653
+                // Default is `id`
+                refreshId: `id`
+            }
+        }
     ]
 };
