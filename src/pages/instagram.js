c32fdefc011db5fe13335eb3d142f8e803cdd696
Puneeth Chaganti <punchagan@muse-amuse.in>
2020-04-13 22:58:22 +0530

    Format instagram.js with Prettier

--- src/pages/instagram.js
+++ src/pages/instagram.js
@@ -1,15 +1,17 @@
-import React from "react"
-import JSONData from "../../content/instagram.json"
+import React from 'react';
+import JSONData from '../../content/instagram.json';
 
 const JSONbuildtime = () => (
-    <div style={{ maxWidth: `960px`, margin: `1.45rem` }}>
+  <div style={{ maxWidth: `960px`, margin: `1.45rem` }}>
     <ul>
-    {JSONData.GraphImages.map((data, index) => {
-        return (<li key={`content_item_${index}`}>
-        <img src={data.display_url} />
-            </li>)
-    })}
+      {JSONData.GraphImages.map((data, index) => {
+        return (
+          <li key={`content_item_${index}`}>
+            <img src={data.display_url} />
+          </li>
+        );
+      })}
     </ul>
-    </div>
-)
-export default JSONbuildtime
+  </div>
+);
+export default JSONbuildtime;
