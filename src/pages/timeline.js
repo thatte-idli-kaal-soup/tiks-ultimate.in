74f7418275d001a3c5fc7bbe4eeb7eec1646eb8a
nikhita-nimbalkar <nikhita.nimbalkar@rakuten.com>
2020-04-14 23:18:50 +0530

    Fetching Timeline details from JSON

--- src/pages/timeline.js
+++ src/pages/timeline.js
@@ -1,70 +1,42 @@
 import React , { Component } from 'react';
 import { VerticalTimeline, VerticalTimelineElement }  from 'react-vertical-timeline-component';
 import 'react-vertical-timeline-component/style.min.css';
-import p1 from '../assets/images/portfolio-1.jpg';
-import p2 from '../assets/images/portfolio-2.jpg';
-import p3 from '../assets/images/portfolio-3.jpg';
-import p4 from '../assets/images/portfolio-4.jpg';
+import eventList from '../../content/timeline.json';
 
 export default class Timeline extends Component {
-    render() {
-      return (
-          <div style={{background:"#D3D3D3"}}>
-        <VerticalTimeline>
-          <VerticalTimelineElement
+
+createTimeline(){
+    return eventList.Events.map((data, index) => {
+        return (
+            <VerticalTimelineElement
             className="vertical-timeline-element--work"
             contentArrowStyle={{ borderRight: '7px solid  rgb(33, 150, 243)' }}
             iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
-            date="2020 - present"
-          >
-            <h3 className="vertical-timeline-element-title">NCS 2 Nationals</h3>
-            <h4 className="vertical-timeline-element-subtitle">Bangalore</h4>
+            date={data.date}
+            >
+            <h3 className="vertical-timeline-element-title">{data.title}</h3>
+            <h4 className="vertical-timeline-element-subtitle">{data.subtitle}</h4>
             <br></br>
-            <img className="img-fluid" src={p1} alt="" />
+            <img className="img-fluid" src={'/timeline-images/'+data.image+'.jpg'} alt="" />
             <p>
-             Final seed - 8th
+             {data.description}
             </p>
-          </VerticalTimelineElement>
-          <VerticalTimelineElement
-            className="vertical-timeline-element--work"
-            date="2018 - 2019"
-            iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}  
-          >
-            <h3 className="vertical-timeline-element-title">NCS 2 Single Gender Regionals</h3>
-            <h4 className="vertical-timeline-element-subtitle">Bangalore</h4>
-            <br></br>
-            <img className="img-fluid" src={p2} alt="" />
-            <p>
-              Final Seed : Opens - 9th , Womens - 3rd
-            </p>
-          </VerticalTimelineElement>
-          <VerticalTimelineElement
-            className="vertical-timeline-element--work"
-            date="2008 - 2010"
-            iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
-          >
-            <h3 className="vertical-timeline-element-title">NCS 2 Mixed Regionals</h3>
-            <h4 className="vertical-timeline-element-subtitle">Bangalore</h4>
-            <br></br>
-            <img className="img-fluid" src={p3} alt="" />
-            <p>
-              Final seed - 8th
-            </p>
-          </VerticalTimelineElement>
-          <VerticalTimelineElement
-            className="vertical-timeline-element--work"
-            date="2006 - 2008"
-            iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
-          >
-            <h3 className="vertical-timeline-element-title">NCS 2 Sectionals</h3>
-            <h4 className="vertical-timeline-element-subtitle">Bangalore</h4>
-            <br></br>
-            <img className="img-fluid" src={p4} alt="" />
-            <p>
-              Final seed - 8th
-            </p>
-          </VerticalTimelineElement>
-        </VerticalTimeline></div>);
+            </VerticalTimelineElement>
+        );
+    })
+    
+}
+
+    render(){
+        return (
+            <div style={{background:"#D3D3D3"}}>
+                <VerticalTimeline>
+                    {this.createTimeline()};
+                </VerticalTimeline>
+            </div>
+        )
+
     }
+
 }
 
