e0c4562968b3fc5493b922da3cc214df7e3e69d9
vkrishnaswam <vivekkrishnaswamy88@gmail.com>
2020-04-15 19:09:12 +0530

    Adding config to run instagram scraper from travisCI

---
+++ push_to_git.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -e
+
+GIT_URL="https://punchagan:"${GITHUB_TOKEN}"@github.com/thatte-idli-kaal-soup/thatteidlikaalsoup.team.git"
+
+# Push to GitHub
+git add content/
+git commit -m "Updating instagram feed from travisci" \
+    --author "punchagan (travisci) <punchagan+travis@muse-amuse.in>"
+git push --quiet "${GIT_URL}" master:master
+
