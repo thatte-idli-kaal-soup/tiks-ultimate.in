964d5d8a514428eabc71224b34f62c3661307247
Puneeth Chaganti <punchagan@muse-amuse.in>
2020-04-15 19:58:19 +0530

    Change Travis commits author information

--- push_to_git.sh
+++ push_to_git.sh
@@ -6,5 +6,5 @@ GIT_URL="https://punchagan:"${GITHUB_TOKEN}"@github.com/thatte-idli-kaal-soup/th
 # Push to GitHub
 git add content/
 git commit -m "Updating instagram feed from TravisCI [skip ci]" \
-    --author "punchagan (travisci) <punchagan+travis@muse-amuse.in>"
+    --author "TIKS Travis Bot <tiks.india.ultimate+travisci@gmail.com>"
 git push --quiet "${GIT_URL}" HEAD:master
