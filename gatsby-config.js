8eccb9a97dd4dc220725ce691816d4da89ff4db6
Puneeth Chaganti <punchagan@muse-amuse.in>
2020-04-15 23:34:28 +0530

    Change the blog post node type

--- gatsby-config.js
+++ gatsby-config.js
@@ -26,9 +26,6 @@ module.exports = {
     {
       resolve: 'gatsby-source-apiserver',
       options: {
-        // Type prefix of entities from server
-        typePrefix: 'blog__',
-
         // The url, this should be the endpoint you are attempting to pull data from
         url: process.env.POSTS_API_URL,
 
@@ -40,7 +37,7 @@ module.exports = {
 
         // Name of the data to be downloaded.  Will show in graphQL or be saved to a file
         // using this name. i.e. posts.json
-        name: `posts`,
+        name: `blogPosts`,
 
         // Optionally include some output when building
         // Default is false
