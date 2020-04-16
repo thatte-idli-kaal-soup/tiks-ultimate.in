8243d32cea38d302a4d1cd210d1e39d39d7ac579
Puneeth Chaganti <punchagan@muse-amuse.in>
2020-04-16 23:28:10 +0530

    index: Remove portfolio images from the gallery

--- src/pages/index.js
+++ src/pages/index.js
@@ -157,63 +157,6 @@ const IndexPage = ({ data }) => (
           {/* <h3 className="text-secondary mb-0">Gallery</h3> */}
           <h2 className="mb-5">Gallery</h2>
         </div>
-        <div className="row no-gutters">
-          <div className="col-lg-6">
-            <a className="portfolio-item" href="/#">
-              <span className="caption">
-                <span className="caption-content">
-                  <h2>Stationary</h2>
-                  <p className="mb-0">
-                    A yellow pencil with envelopes on a clean, blue backdrop!
-                  </p>
-                </span>
-              </span>
-              <img className="img-fluid" src={p1} alt="" />
-            </a>
-          </div>
-          <div className="col-lg-6">
-            <a className="portfolio-item" href="/#">
-              <span className="caption">
-                <span className="caption-content">
-                  <h2>Ice Cream</h2>
-                  <p className="mb-0">
-                    A dark blue background with a colored pencil, a clip, and a
-                    tiny ice cream cone!
-                  </p>
-                </span>
-              </span>
-              <img className="img-fluid" src={p2} alt="" />
-            </a>
-          </div>
-          <div className="col-lg-6">
-            <a className="portfolio-item" href="/#">
-              <span className="caption">
-                <span className="caption-content">
-                  <h2>Strawberries</h2>
-                  <p className="mb-0">
-                    Strawberries are such a tasty snack, especially with a
-                    little sugar on top!
-                  </p>
-                </span>
-              </span>
-              <img className="img-fluid" src={p3} alt="" />
-            </a>
-          </div>
-          <div className="col-lg-6">
-            <a className="portfolio-item" href="/#">
-              <span className="caption">
-                <span className="caption-content">
-                  <h2>Workspace</h2>
-                  <p className="mb-0">
-                    A yellow workspace with some scissors, pencils, and other
-                    objects.
-                  </p>
-                </span>
-              </span>
-              <img className="img-fluid" src={p4} alt="" />
-            </a>
-          </div>
-        </div>
       </div>
       <InstagramGrid imageData={JSONData} />
     </section>
