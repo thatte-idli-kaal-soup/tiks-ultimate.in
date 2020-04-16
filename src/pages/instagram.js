060ec980b604357fe7ad1d39742e7a308addf2eb
Puneeth Chaganti <punchagan@muse-amuse.in>
2020-04-16 19:37:04 +0530

    Display the Instagram images in a grid
    The tall InstagramTV image still needs to be handled correctly.

--- src/pages/instagram.js
+++ src/pages/instagram.js
@@ -1,24 +1,54 @@
 import React from 'react';
 import JSONData from '../../content/thatteidlikaalsoup.json';
 
-const JSONbuildtime = () => (
-  <div style={{ maxWidth: `960px`, margin: `1.45rem` }}>
-    <ul>
-      {JSONData.GraphImages.map((data, index) => {
-        const text =
-          (data.edge_media_to_caption.edges.length > 0 &&
-            data.edge_media_to_caption.edges[0].node.text) ||
-          '';
+const constructImageGrid = images => {
+  const imageGrid = [];
+  let row,
+    rowIndex = -1;
+  JSONData.GraphImages.map((image, index) => {
+    if (index % 3 == 0) {
+      row = [];
+      rowIndex += 1;
+      imageGrid.push(row);
+    } else {
+      row = imageGrid[rowIndex];
+    }
+    row.push(image);
+  });
+  return imageGrid;
+};
+
+const JSONbuildtime = () => {
+  const images = constructImageGrid(JSONData.GraphImages);
+  return (
+    <div className="container">
+      {images.map((imageRow, rowIndex) => {
         return (
-          <li key={`content_item_${index}`}>
-            <a href={`https://www.instagram.com/p/${data.shortcode}`}>
-              <img src={data.thumbnail_resources[3].src} alt={text} />
-            </a>
-            <p>{text}</p>
-          </li>
+          <div className="row" key={`image_row_${rowIndex}`}>
+            {imageRow.map(data => {
+              const text =
+                (data.edge_media_to_caption.edges.length > 0 &&
+                  data.edge_media_to_caption.edges[0].node.text) ||
+                '';
+              return (
+                <div
+                  className="col-4 my-2"
+                  key={`content_item_${data.shortcode}`}
+                >
+                  <a href={`https://www.instagram.com/p/${data.shortcode}`}>
+                    <img
+                      className="img-fluid"
+                      src={data.thumbnail_resources[2].src}
+                      alt={text}
+                    />
+                  </a>
+                </div>
+              );
+            })}
+          </div>
         );
       })}
-    </ul>
-  </div>
-);
+    </div>
+  );
+};
 export default JSONbuildtime;
