b9a995eb07eaf79f0ac7b2987117651ee1f1f839
Puneeth Chaganti <punchagan@muse-amuse.in>
2020-05-10 16:51:51 +0530

    push_to_git: Remove [skip ci] from commit message
    Travis now only runs cron jobs and doesn't get triggered on [skip ci].
    We can remove this from the commit message. This is probably stopping
    Netlify from building the commits.

--- push_to_git.sh
+++ push_to_git.sh
@@ -5,6 +5,6 @@ GIT_URL="https://punchagan:"${GITHUB_TOKEN}"@github.com/thatte-idli-kaal-soup/th
 
 # Push to GitHub
 git add content/
-git commit -m "Updating instagram feed from TravisCI [skip ci]" \
+git commit -m "Updating instagram feed from TravisCI " \
     --author "TIKS Travis Bot <tiks.india.ultimate+travisci@gmail.com>"
 git push --quiet "${GIT_URL}" HEAD:master
