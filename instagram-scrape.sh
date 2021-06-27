14744affdab3e38f59671ec1060d09984e31e5ff
Puneeth Chaganti <punchagan@muse-amuse.in>
2021-06-27 17:41:32 +0530

    workflows: Remove --latest argument to instagram-scraper

--- instagram-scrape.sh
+++ instagram-scrape.sh
@@ -3,7 +3,7 @@ set -e
 
 pushd $(dirname $0)
 source .envrc
-instagram-scraper --latest --media-metadata --media-types='none' -d content tiks_ultimate
+instagram-scraper --media-metadata --media-types='none' -d content tiks_ultimate
 git add content/
 git commit -m "Updating instagram feed in a cron" \
     --author "TIKS Cron Job <tiks.india.ultimate+cronjob@gmail.com>"
