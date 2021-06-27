e743a87cd7fa1fb6a97a4068edd2258597590872
Puneeth Chaganti <punchagan@muse-amuse.in>
2021-06-27 18:35:46 +0530

    instagram: Download thumbnails since Instagram now has stricter no-cors

--- instagram-scrape.sh
+++ instagram-scrape.sh
@@ -1,11 +1,33 @@
 #!/bin/bash
-set -e
+set -eu
+
+function download_url {
+    mkdir -p "static/thumbnails/"
+    url="${1}"
+    filename=$(basename $(echo "${url}" | cut -d ? -f 1))
+    wget --quiet --continue --no-clobber "${url}" --output-document "static/thumbnails/${filename}" || true
+}
+
+function download_thumbnails {
+    urls=$(jq -r '.GraphImages[].thumbnail_src' content/tiks_ultimate.json)
+    for url in ${urls};
+    do
+        download_url ${url}
+    done
+}
+
+function fetch_instagram {
+    instagram-scraper --media-metadata --media-types='none' -d content tiks_ultimate
+    download_thumbnails
+    git add content/
+    git add static/thumbnails/
+    git commit -m "Updating instagram feed in a cron" \
+        --author "TIKS Cron Job <tiks.india.ultimate+cronjob@gmail.com>"
+}
+
 
 pushd $(dirname $0)
 source .envrc
-instagram-scraper --media-metadata --media-types='none' -d content tiks_ultimate
-git add content/
-git commit -m "Updating instagram feed in a cron" \
-    --author "TIKS Cron Job <tiks.india.ultimate+cronjob@gmail.com>"
+fetch_instagram
 git pull --rebase origin master
 git push origin master
