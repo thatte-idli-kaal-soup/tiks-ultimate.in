af93034e9ba0f5727b4ed95f9505046ec555be38
Puneeth Chaganti <punchagan@muse-amuse.in>
2021-06-27 17:39:16 +0530

    workflows: Change commit author name in instagram-scrape.sh

--- instagram-scrape.sh
+++ instagram-scrape.sh
@@ -6,6 +6,6 @@ source .envrc
 instagram-scraper --latest --media-metadata --media-types='none' -d content tiks_ultimate
 git add content/
 git commit -m "Updating instagram feed in a cron" \
-    --author "TIKS Travis Bot <tiks.india.ultimate+cronjob@gmail.com>"
+    --author "TIKS Cron Job <tiks.india.ultimate+cronjob@gmail.com>"
 git pull --rebase origin master
 git push origin master
