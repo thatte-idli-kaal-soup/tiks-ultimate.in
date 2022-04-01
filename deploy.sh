19c88f28a66990733f2b26c44793e84fdbaa6444
Puneeth Chaganti <punchagan@muse-amuse.in>
2022-04-01 22:55:21 +0530

    Extract build and deploy functions in deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,27 +3,32 @@ set -e
 
 PUBLIC_DIR="public"
 
-if [ -z "${GITHUB_TOKEN}" ]
-then
-    GIT_URL=$(git remote get-url origin)
-else
-    GIT_URL="https://github.com/thatte-idli-kaal-soup/thatteidlikaalsoup.team.git"
-    GIT_URL=$(echo $GIT_URL|sed -e s/github.com/punchagan:"${GITHUB_TOKEN}"@github.com/g)
-fi
+# Push to GitHub
+deploy () {
+    if [ -z "${GITHUB_TOKEN}" ]
+    then
+        GIT_URL=$(git remote get-url origin)
+    else
+        GIT_URL="https://github.com/thatte-idli-kaal-soup/thatteidlikaalsoup.team.git"
+        GIT_URL=$(echo $GIT_URL|sed -e s/github.com/punchagan:"${GITHUB_TOKEN}"@github.com/g)
+    fi
+    pushd "${PUBLIC_DIR}"
+    git init
+    git config user.email "punchagan+ghactions@muse-amuse.in"
+    git config user.name "punchagan (gh-actions)"
+    git add .
+    git commit -m "Deploy to GitHub Pages"
+    git push --force --quiet "${GIT_URL}" master:gh-pages
+    popd
+}
 
 # Build the site
+build () {
+    rm -rf "${PUBLIC_DIR}"
+    hugo
+}
+
 pushd $(dirname $0)
-rm -rf "${PUBLIC_DIR}"
-hugo
-
-# Push to GitHub
-pushd "${PUBLIC_DIR}"
-git init
-git config user.email "punchagan+ghactions@muse-amuse.in"
-git config user.name "punchagan (gh-actions)"
-git add .
-git commit -m "Deploy to GitHub Pages"
-git push --force --quiet "${GIT_URL}" master:gh-pages
-popd
-
+build
+deploy
 popd
